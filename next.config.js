cad478332c783026e412a99ddb380d2860d4e3e6
readyyyk <mr.sairexer@gmail.com>
2024-03-19 12:41:56 +0300

    trying to deploy

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ await import('./src/env.js');
 /** @type {import("next").NextConfig} */
 const config = {
     output: 'export',
+    distDir: 'dist',
     images: {
         dangerouslyAllowSVG: true,
         remotePatterns: [
